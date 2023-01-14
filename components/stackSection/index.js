6b930d9f2ffd19f2fca965c1421df50914efab14
Leonardll <lonardlator@yahoo.fr>
2023-01-14 06:28:37 +0100

    🐛

--- components/stackSection/index.js
+++ components/stackSection/index.js
@@ -19,8 +19,6 @@ function CardItem({ cardUrl, cardTitle }) {
     )
 }
 const StackSection = (stackData, loader) => {
-    console.log(stackData)
-
     return (
         <section id="stack">
             <div className="container  my-4 mx-auto">
@@ -34,7 +32,7 @@ const StackSection = (stackData, loader) => {
                 />
 
                 <div className="grid grid-cols-2 auto-rows-fr md:grid-cols-4 gap-2 md:gap-4 place-items-stretch place-content-center m-5">
-                    {stackData.stackData.data.map((card) => (
+                    {stackData.stackData.data?.map((card) => (
                         <CardItem
                             key={card._id}
                             cardUrl={card.url}
--- out/static/chunks/webpack.js
+++ out/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e8ba13738c4c8efc"; }
+/******/ 		__webpack_require__.h = function() { return "f306028091a87541"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
