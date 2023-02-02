8cf7a127d64ead6ff915b7af6ed9a31df8f7b59e
Leonardll <lonardlator@yahoo.fr>
2023-02-02 22:22:02 +0100

    💄 meta tag prop added

--- out/static/chunks/webpack.js
+++ out/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "af54055ecd33ea5a"; }
+/******/ 		__webpack_require__.h = function() { return "86f934c700d944bb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
