b7dd7b9d8c0dfdc4e4d6906baa99429d6d3fd629
Leonardll <lonardlator@yahoo.fr>
2023-02-03 02:02:25 +0100

    ➕ next analytics

--- out/static/chunks/webpack.js
+++ out/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8985fd6d15e300ae"; }
+/******/ 		__webpack_require__.h = function() { return "f8775f578046b7e6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
